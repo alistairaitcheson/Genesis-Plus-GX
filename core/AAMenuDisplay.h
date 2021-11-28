7e9f4f0bed723c733c0a9a6e8e3d0a96659a988d
alistairaitcheson <a.g.aitcheson@gmail.com>
2021-11-28 14:58:38 +0000

    fix accidental bug

--- core/AAMenuDisplay.h
+++ core/AAMenuDisplay.h
@@ -56,6 +56,7 @@ typedef struct {
     int ramWritesPerRing; // 0 = off, 1 = 1x, 2 = 5x, 3 = 25x, 4 = 100x
     int ramWriteStartLoc[4];
     int ramWriteEndLoc[4];
+    int shouldSaveRewindStates;
     int vramWritesPerRing; // 0 = off, 1 = 1x, 2 = 5x, 3 = 25x, 4 = 100x
 
 } SecondaryHackOptions;
