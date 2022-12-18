f402552476c1e8e60d7f7d9e964271adac07f486
alistairaitcheson <a.g.aitcheson@gmail.com>
2022-12-18 00:30:50 +0000

    comment out test stuff

--- core/AAModConsole.c
+++ core/AAModConsole.c
@@ -403,9 +403,9 @@ void checkForBossHits() {
                     }
 
                     // for testing - quick kills!
-                    if (aa_genesis_getWorkRam(locationToCheck) > 1){
-                         aa_genesis_setWorkRam(locationToCheck, 1);
-                    }
+                    // if (aa_genesis_getWorkRam(locationToCheck) > 1){
+                    //      aa_genesis_setWorkRam(locationToCheck, 1);
+                    // }
 
                     // char rushText[0x40];
                     // sprintf(rushText, "%04X %02X", locationToCheck, aa_genesis_getWorkRam(locationToCheck));
