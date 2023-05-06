d52ebf458b065a2463046e7df3dcff824e4732d6
alistairaitcheson <a.g.aitcheson@gmail.com>
2023-05-06 09:50:16 +0100

    v37?

--- core/AAMenuDisplay.c
+++ core/AAMenuDisplay.c
@@ -35,7 +35,7 @@ static int gameSuiteSelectIndex = 0;
 static int multithreadingOptionsIndex = 0;
 
 static int majorVersion = 0;
-static int minorVersion = 36;
+static int minorVersion = 37;
 
 static int DEFAULT_WIDTH = 320;
 static int DEFAULT_HEIGHT = 200;
