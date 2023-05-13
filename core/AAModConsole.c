b7b7e6a105226c45682a17b9037ac9f4e5541d33
alistairaitcheson <a.g.aitcheson@gmail.com>
2023-05-13 11:48:05 +0100

    remove debug logs

--- core/AAModConsole.c
+++ core/AAModConsole.c
@@ -1391,13 +1391,13 @@ void modConsole_updateFrame() {
                     applyBossRushCachedRings();
                     if (aa_genesis_getWorkRam(0xF601) == 0x0C || ninesStage.gameId == 6) {
                         countdownToApplyBossRushRings--;
-                        layerRenderer_fill(3, 100, 18, 100, 2, 0xFF);
+                        // layerRenderer_fill(3, 100, 18, 100, 2, 0xFF);
                     }
                 } else if (aa_genesis_getWorkRam(gameTransferListing.ringBytesForTransfer[0]) > 0 
                     || aa_genesis_getWorkRam(gameTransferListing.ringBytesForTransfer[1]) > 0
                     || aa_genesis_getWorkRam(damageBoostIndex) > damageBoostMaximum) {
                     cacheRingCountInBossRush(1);
-                    layerRenderer_fill(3, 0, 18, 300, 2, 0xFF);
+                    // layerRenderer_fill(3, 0, 18, 300, 2, 0xFF);
 
                     if (getBossRushRingCarryTotal() == 0) {
                         ringsWentToZero = 1;
@@ -1411,23 +1411,23 @@ void modConsole_updateFrame() {
                 unsigned int levelEndLocation = ninesParams.levelCompleteLocation;
                 unsigned int levelEndValue = ninesParams.levelCompleteValue;
 
-                char tempLog[256];
-                sprintf(tempLog,"--> (%04X - %02X) (%04X - %02X / %02X) - ", 
-                    levelSwitchIndex, aa_genesis_getWorkRam(levelSwitchIndex),
-                    levelEndLocation, aa_genesis_getWorkRam(levelEndLocation), levelEndValue);
-                cartLoader_appendToLog(tempLog);
+                // char tempLog[256];
+                // sprintf(tempLog,"--> (%04X - %02X) (%04X - %02X / %02X) - ", 
+                //     levelSwitchIndex, aa_genesis_getWorkRam(levelSwitchIndex),
+                //     levelEndLocation, aa_genesis_getWorkRam(levelEndLocation), levelEndValue);
+                // cartLoader_appendToLog(tempLog);
 
-                char tempLog2[256];
-                sprintf(tempLog2,"     %04X %04X / %02X %02X / %02X %02X - %i", 
-                    gameTransferListing.ringBytesForTransfer[0], gameTransferListing.ringBytesForTransfer[1],
-                    aa_genesis_getWorkRam(gameTransferListing.ringBytesForTransfer[0]), aa_genesis_getWorkRam(gameTransferListing.ringBytesForTransfer[1]),
-                    getBossRushRingCarryValues(0), getBossRushRingCarryValues(1), getBossRushRingCarryValues(2));
-                cartLoader_appendToLog(tempLog2);
+                // char tempLog2[256];
+                // sprintf(tempLog2,"     %04X %04X / %02X %02X / %02X %02X - %i", 
+                //     gameTransferListing.ringBytesForTransfer[0], gameTransferListing.ringBytesForTransfer[1],
+                //     aa_genesis_getWorkRam(gameTransferListing.ringBytesForTransfer[0]), aa_genesis_getWorkRam(gameTransferListing.ringBytesForTransfer[1]),
+                //     getBossRushRingCarryValues(0), getBossRushRingCarryValues(1), getBossRushRingCarryValues(2));
+                // cartLoader_appendToLog(tempLog2);
 
                 if (countdownToApplyBossRushRings == 0) {
-                    layerRenderer_fill(3, 0, 0, 300, 16, 0xFF);
-                    layerRenderer_writeWord256(3, 0, 0, tempLog, 0x5);
-                    layerRenderer_writeWord256(3, 0, 8, tempLog2, 0x5);
+                    // layerRenderer_fill(3, 0, 0, 300, 16, 0xFF);
+                    // layerRenderer_writeWord256(3, 0, 0, tempLog, 0x5);
+                    // layerRenderer_writeWord256(3, 0, 8, tempLog2, 0x5);
 
                     // check for load trigger changed
                     if ((aa_genesis_getWorkRam(levelSwitchIndex) >= 0x80
