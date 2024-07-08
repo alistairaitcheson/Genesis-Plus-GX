e5041b74c1055330c3008cd5a215ac6df1b08b87
alistairaitcheson <a.g.aitcheson@gmail.com>
2024-07-08 11:28:42 +0100

    level select in mickey

--- core/AACartLoader.c
+++ core/AACartLoader.c
@@ -955,8 +955,10 @@ void cartLoader_run() {
     writeStringToArray32("MICKEYMANIA", gameListings[43].gameId);
     gameListings[43].ringByte = 0x0470; // marbles
     gameListings[43].livesBytes[0] = 0x0466; // lives
+    gameListings[43].livesBytes[1] = 0x045A; // level select
     gameListings[43].livesByteDestinations[0] = 0x5;
-    
+    gameListings[43].livesByteDestinations[1] = 0x1;
+
     writeStringToArray32("MRNUTZ", gameListings[44].gameId);
     scoreMonitorListings[44].scoreBytes[0] = 0xE84E; // something that seems to change when you kill an enemy
     scoreMonitorListings[44].scoreBytes[1] = 0xE84F; // (sometimes just once, sometimes it's a countdown)
